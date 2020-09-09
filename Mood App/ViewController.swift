68757179a93229d95391d8070873c820c73f4425
“DiggyShants” <dalton.flanagan@gmail.com>
2020-09-09 17:36:00 +0800

    Initial Commit

---
+++ Mood App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mood App
+//
+//  Created by Dalton Flanagan on 9/9/2020.
+//  Copyright © 2020 Dalton Flanagan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mood AppTests/Mood_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mood_AppTests.swift
+//  Mood AppTests
+//
+//  Created by Dalton Flanagan on 9/9/2020.
+//  Copyright © 2020 Dalton Flanagan. All rights reserved.
+//
+
+import XCTest
+@testable import Mood_App
+
+class Mood_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
